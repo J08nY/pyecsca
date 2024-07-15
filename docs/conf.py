0eff8d9da4fbe449e314fd910c3ec5935bc4d346
J08nY <johny@neuromancer.sk>
2024-07-15 15:26:23 +0200

    Bump to 0.3.1.

--- docs/conf.py
+++ docs/conf.py
@@ -26,9 +26,9 @@ author = 'Jan Jancar'
 sys.path.append(os.path.abspath('..'))
 
 # The short X.Y version
-version = '0.3.0'
+version = '0.3.1'
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+release = '0.3.1'
 
 
 # -- General configuration ---------------------------------------------------
